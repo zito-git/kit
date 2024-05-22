5a33e8d3cb589d2345ad85227b432d915815e310
zito-git <strongpower.zito@gmail.com>
2024-05-22 23:44:09 +0900

    add

---
+++ 2024-1/web-team/api/member.js
@@ -0,0 +1,105 @@
+const db = require("../config/db");
+const bcrypt = require("bcrypt");
+
+function useid(req, res, next) {
+  const r = req.body;
+  const arr = [r.userid];
+
+  db.connection.query(
+    "SELECT * FROM `member` WHERE `userid` = ?",
+    arr[0],
+    function (err, results, fields) {
+      if (results[0] === undefined) {
+        const result = {
+          status: "empty",
+          msg: "사용가능한 아이디 입니다.",
+        };
+        res.json(result);
+        return;
+      } else {
+        const result = {
+          status: "not empty",
+          msg: "이미 존재하는 아이디 입니다.",
+        };
+        res.json(result);
+        return;
+      }
+    }
+  );
+}
+
+function register(req, res, next) {
+  const r = req.body;
+  const encryptedPW = bcrypt.hashSync(r.userpw, 10);
+  const arr = [r.nickname, r.userid, encryptedPW, r.role];
+
+  // 유저가 존재하는지 확인
+  db.connection.query(
+    "SELECT * FROM `member` WHERE `userid` = ?",
+    arr[1],
+    function (err, results, fields) {
+      if (results[0] !== undefined) {
+        let result01 = {
+          status: "not empty",
+          msg: "이미 존재하는 아이디 입니다.",
+        };
+        res.json(result01);
+        return;
+      } else {
+        //회원생성
+        const sql =
+          "INSERT INTO `member`(`nickname`, `userid`, `userpw`, `role`) VALUES (?,?,?,?)";
+
+        db.connection.query(sql, arr, (err, result, fields) => {
+          let result02 = {
+            status: "success",
+            msg: "회원가입이 완료 되었습니다.",
+          };
+          res.json(result02);
+          return;
+        });
+      }
+    }
+  );
+}
+
+function login(req, res, next) {
+  const r = req.body;
+  const arr = [r.userid, r.userpw];
+
+  db.connection.query(
+    "SELECT * FROM `member` WHERE `userid` = ?",
+    arr[0],
+    function (err, results, fields) {
+      if (results[0] === undefined) {
+        const result = {
+          status: "no user",
+          msg: "존재하지 않는 사용자 입니다.",
+        };
+        res.json(result);
+        return;
+      } else {
+        // 회원이 존재하고 비밀번호가 일치하는지 확인
+        const same = bcrypt.compareSync(arr[1], results[0].userpw);
+
+        if (arr[0] === results[0].userid && same === true) {
+          const result = {
+            status: "success",
+            msg: "로그인 성공!",
+          };
+          res.json(result);
+          return;
+        } else {
+          const result = {
+            status: "fail",
+            msg: "로그인 실패",
+          };
+          res.json(result);
+          return;
+        }
+      }
+    }
+  );
+}
+
+module.exports = { useid, register, login };
--- 2024-1/web-team/routes/api.js
+++ 2024-1/web-team/routes/api.js
@@ -1,26 +1,13 @@
 var express = require("express");
 var router = express.Router();
 var db = require("../config/db");
+const bcrypt = require("bcrypt");
+const memberApi = require("../api/member");
 
-router.get("/login", function (req, res, next) {
-  // db.connection.query("SELECT * FROM member", function (err, results, fields) {
-  //   console.log(results);
-  // });
-  res.send("O");
-});
+router.post("/login", memberApi.login);
 
-router.post("/register", function (req, res, next) {
-  const r = req.body;
-  const arr = [r.nickname, r.userid, r.userpw, r.role];
+router.post("/useid", memberApi.useid);
 
-  const resultJson = {
-    nickname: arr[0],
-    userid: arr[1],
-    userpw: arr[2],
-    role: arr[3],
-  };
-
-  res.json(resultJson);
-});
+router.post("/register", memberApi.register);
 
 module.exports = router;
