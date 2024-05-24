c3be12eda414f2c6f78843a9574861a6d0fcbf44
zito-git <strongpower.zito@gmail.com>
2024-05-24 12:05:46 +0900

    update
    index페이지 접속시 api페이지로 redirect

--- 2024-1/web-team/routes/index.js
+++ 2024-1/web-team/routes/index.js
@@ -1,11 +1,7 @@
 var express = require("express");
 var router = express.Router();
 router.get("/", function (req, res, next) {
-  const response = {
-    status: "err",
-    msg: "index page",
-  };
-  res.json(response);
+  res.redirect("/api");
 });
 
 module.exports = router;
