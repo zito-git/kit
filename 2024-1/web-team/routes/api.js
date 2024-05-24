3ec3e6756ff9c35739f09bcb01c28ae37185ac56
zito-git <strongpower.zito@gmail.com>
2024-05-24 11:26:47 +0900

    index페이지 테스트용
    16라인

--- 2024-1/web-team/routes/api.js
+++ 2024-1/web-team/routes/api.js
@@ -4,6 +4,28 @@ var db = require("../config/db");
 const bcrypt = require("bcrypt");
 const memberApi = require("../api/member");
 
+//인덱스 페이지 임시
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+
+router.get("/", function (req, res, next) {
+  res.send("API PAGE");
+});
+
+//인덱스페이지 - 나중에 분리
+router.get("/home", function (req, res, next) {
+  const token = req.header("Token");
+  try {
+    const verified = jwt.verify(token, process.env.ENV_SKEY);
+  } catch {
+    const emptyToken = {
+      status: "null",
+      msg: "토큰이 없거나 잘못 되었습니다.",
+    };
+    res.send(emptyToken);
+  }
+});
+
 router.post("/login", memberApi.login);
 
 router.post("/useid", memberApi.useid);
