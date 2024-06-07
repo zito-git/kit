90bfe4653ab1b086bb068128327b0cc072a4ac9a
zito-git <iam@zito.kr>
2024-06-07 21:17:27 +0900

    update : 메인페이지 토큰제거

--- 2024-1/web-team/api/home.js
+++ 2024-1/web-team/api/home.js
@@ -4,16 +4,16 @@ const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 function index(req, res, next) {
-  const token = req.header("Token");
-  try {
-    const verified = jwt.verify(token, process.env.ENV_SKEY);
-  } catch {
-    const emptyToken = {
-      status: "null",
-      msg: "토큰이 없거나 잘못 되었습니다.",
-    };
-    return res.send(emptyToken);
-  }
+  // const token = req.header("Token");
+  // try {
+  //   const verified = jwt.verify(token, process.env.ENV_SKEY);
+  // } catch {
+  //   const emptyToken = {
+  //     status: "null",
+  //     msg: "토큰이 없거나 잘못 되었습니다.",
+  //   };
+  //   return res.send(emptyToken);
+  // }
   db.connection.query("SELECT * FROM `camp`", function (err, results, fields) {
     return res.json(results);
   });
