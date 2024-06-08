05503152632d91fe3db472cb4527303a0e52f68a
zito-git <iam@zito.kr>
2024-06-08 17:02:56 +0900

    update : 코드정리

--- 2024-1/web-team/routes/api.js
+++ 2024-1/web-team/routes/api.js
@@ -4,14 +4,14 @@ var db = require("../config/db");
 const bcrypt = require("bcrypt");
 const memberApi = require("../api/member");
 const homeApi = require("../api/home");
-
+const shopApi = require("../api/shop");
 const multer = require("multer");
-
 const path = require("path");
 
+//업로드
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "public/img"); // cb 콜백함수를 통해 전송된 파일 저장 디렉토리 설정
+    cb(null, "public/assets"); // cb 콜백함수를 통해 전송된 파일 저장 디렉토리 설정
   },
   filename: function (req, file, cb) {
     cb(null, new Date().valueOf() + path.extname(file.originalname)); // cb 콜백함수를 통해 전송된 파일 이름 설정
@@ -19,44 +19,11 @@ var storage = multer.diskStorage({
 });
 
 const upload = multer({
-  dest: "public/img/",
+  dest: "public/assets",
   storage: storage,
 });
 
-router.post("/shop/add", upload.array("image"), function (req, res, next) {
-  // 토큰검사
-  const token = req.header("Token");
-  try {
-    const verified = jwt.verify(token, process.env.ENV_SKEY);
-  } catch {
-    const emptyToken = {
-      status: "null",
-      msg: "토큰이 없거나 잘못 되었습니다.",
-    };
-    return res.send(emptyToken);
-  }
-
-  const {
-    name,
-    site,
-    address,
-    phone,
-    intro,
-    check_in,
-    check_out,
-    charge,
-    member,
-    area_info,
-  } = req.body;
-
-  for (let i = 0; i < req.files.length; i++) {
-    console.log(req.files[i].filename);
-  }
-  console.log(req.body);
-  res.send("OK");
-});
-
-//인덱스 페이지 임시
+//토큰검사 임시
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
 
@@ -64,7 +31,7 @@ router.get("/", function (req, res, next) {
   res.send("info -> DISCORD API");
 });
 
-//인덱스페이지
+//
 router.get("/home", homeApi.index);
 
 //멤버페이지
@@ -74,37 +41,7 @@ router.post("/useid", memberApi.useid);
 
 router.post("/register", memberApi.register);
 
-// router.post("/seller/reservation", memberApi.register);
-
-//판매자(seller)페이지
-
-// router.post("/shop/add", function (req, res, next) {
-// // 토큰검사
-// const token = req.header("Token");
-// try {
-//   const verified = jwt.verify(token, process.env.ENV_SKEY);
-// } catch {
-//   const emptyToken = {
-//     status: "null",
-//     msg: "토큰이 없거나 잘못 되었습니다.",
-//   };
-//   return res.send(emptyToken);
-// }
-
-//   // const {
-//   //   name,
-//   //   address,
-//   //   phone,
-//   //   intro,
-//   //   check_in,
-//   //   check_out,
-//   //   charge,
-//   //   member,
-//   //   area_info,
-//   // } = req.body;
-
-//   // 정보추가
-//   res.send("ddd");
-// });
+// 매장
+router.post("/shop/add", upload.array("image"), shopApi.addShopInfo);
 
 module.exports = router;
